e2660e01c6ddc03f852f004b5a052a8dcb776c6a
Ostap Kucher <Ostap Kucher@WORKFLOW>
2026-08-12 06:52:03 +0300

    keys finished

--- scripts/bronze/creating keys.sql
+++ scripts/bronze/creating keys.sql
@@ -32,6 +32,11 @@ ALTER TABLE bronze.olist_sellers_dataset
   PRIMARY KEY (seller_id);
 
 
+ALTER TABLE bronze.product_category_name_translation
+  ADD CONSTRAINT pk_product_category_name_translation
+  PRIMARY KEY (product_category_name);
+
+
 /*================
 FOREIGN KEYS
 ================*/
@@ -73,4 +78,27 @@ ALTER TABLE bronze.olist_orders_dataset
   ADD CONSTRAINT fk_olist_orders_dataset__olist_customers_dataset
   FOREIGN KEY (customer_id)
   REFERENCES bronze.olist_customers_dataset (customer_id);
+
+
+-- olist_products_dataset
+ALTER TABLE bronze.olist_products_dataset
+  ADD CONSTRAINT fk_olist_products_dataset__product_category_name_translation
+  FOREIGN KEY (product_category_name)
+  REFERENCES bronze.product_category_name_translation (product_category_name)
+
+
+
+-- Checking for unmatched values
+SELECT DISTINCT
+  p.product_category_name product,
+  t.product_category_name translation
+FROM bronze.olist_products_dataset p
+FULL JOIN bronze.product_category_name_translation t 
+ON t.product_category_name = p.product_category_name;
+
+-- Adding missing values
+INSERT INTO bronze.product_category_name_translation (product_category_name, product_category_name_english)
+VALUES 
+    ('pc_gamer', 'pc_gamer'),
+    ('portateis_cozinha_e_preparadores_de_alimentos', 'portable_kitchen_and_food_preparers')
 
